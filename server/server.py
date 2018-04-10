cc1de6a7b3433e970a33da802a37bba4d0564c93
mark <marktmmrmn@gmail.com>
2018-04-10 13:39:44 +0200

    LAN test

--- server/server.py
+++ server/server.py
@@ -9,7 +9,26 @@ import pickle
 import threading
 from app import ecv
 import sys
-
+presetsLAN = [
+    {
+        'server': 5555,
+        'clients': [
+            '192.168.1.100'
+        ],
+        'flask': 5000,
+        'db': 'sqlite:///foodshare0.db',
+        'queue_name': 'foodshare0'
+    },
+    {
+        'server': 5555,
+        'clients': [
+            '192.168.1.101'
+        ],
+        'flask': 5000,
+        'db': 'sqlite:///foodshare1.db',
+        'queue_name': 'foodshare1'
+    }
+]
 presets = [
     {
         'server': 5555,
@@ -53,13 +72,14 @@ create_engine_and_session(ecv, presets[setting]['db'])
 
 # Start listening on socket
 server = SocketServer(presets[setting]['server']) 
-client1 = SocketClient('127.0.0.1', presets[setting]['clients'][0])
-client2 = SocketClient('127.0.0.1', presets[setting]['clients'][1])
+client1 = SocketClient(presetsLAN[setting]['clients'][0], 5555)
+# client2 = SocketClient('127.0.0.1', presets[setting]['clients'][1])
 
 # if setting == 0:
 #     clients = [client2] # Add clients here to test network sockets
 # elif setting == 1:
-clients = [client1, client2]
+# clients = [client1, client2]
+clients = [client1]
 
 sync = Synchronizer(server, clients)
 
